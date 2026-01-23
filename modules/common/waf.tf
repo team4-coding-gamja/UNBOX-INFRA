5fdce674d7d99cc75403c89ac0bf406ff6b42cf9
nacgyun <nacgyun001@gmail.com>
2026-01-23 17:29:41 +0900

    add WAF 2 prod

---
+++ modules/common/waf.tf
@@ -0,0 +1,52 @@
+resource "aws_wafv2_web_acl" "main" {
+  name        = "${var.project_name}-${var.env}-waf"
+  description = "OWASP Top 10 Managed Rule Set"
+  scope       = "REGIONAL" # ALB 연결용
+
+  default_action {
+    allow {}
+  }
+
+  # 딱 이거 하나! Core Rule Set (CRS)
+  rule {
+    name     = "AWS-AWSManagedRulesCommonRuleSet"
+    priority = 1
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        vendor_name = "AWS"
+        name        = "AWSManagedRulesCommonRuleSet"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "CommonRuleSetMetric"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "unbox-waf-metric"
+    sampled_requests_enabled   = true
+  }
+}
+
+# [필수] ALB와 WAF를 연결해주는 코드
+resource "aws_wafv2_web_acl_association" "main" {
+  resource_arn = var.alb_arn # ALB의 ARN을 넣어줘야 합니다.
+  web_acl_arn  = aws_wafv2_web_acl.main.arn
+}
+
+resource "aws_wafv2_web_acl_logging_configuration" "main" {
+  resource_arn = aws_wafv2_web_acl.main.arn
+
+  log_destination_configs = [
+    aws_kinesis_firehose_delivery_stream.waf_logs.arn
+  ]
+}
