09fd6ae64cf758acf4ccb6d91f4992eeeb2d9ecb
nacgyun <nacgyun001@gmail.com>
2026-02-11 18:58:30 +0900

    add URL in env

--- terraform/modules/security_group/main.tf
+++ terraform/modules/security_group/main.tf
@@ -31,11 +31,7 @@ resource "aws_security_group" "msk" {
   vpc_id = var.vpc_id
 }
 
-resource "aws_security_group" "eks_node" {
-  name   = "${var.project_name}-${var.env}-eks-node-sg"
-  vpc_id = var.vpc_id
-  tags   = { Name = "${var.project_name}-${var.env}-eks-node-sg" }
-}
+
 
 resource "aws_security_group" "nat" {
   count  = var.env != "prod" ? 1 : 0
@@ -223,21 +219,7 @@ resource "aws_security_group_rule" "eks_node_ingress_from_alb" {
 }
 
 # EKS Node Inbound (Self - for pod-to-pod communication)
-resource "aws_security_group_rule" "eks_node_ingress_self" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  self              = true
-  security_group_id = aws_security_group.eks_node.id
-}
+
 
 # EKS Node Outbound (All traffic)
-resource "aws_security_group_rule" "eks_node_egress_all" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.eks_node.id
-}
+
